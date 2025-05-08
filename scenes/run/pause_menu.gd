6ea39d467bb89b42d1aec3e7787fd88124e1b8cb
kino <1491037864@qq.com>
2025-05-08 15:29:18 +0800

    add a pause panel

--- scenes/run/pause_menu.gd
+++ scenes/run/pause_menu.gd
@@ -5,11 +5,26 @@ signal save_and_quit
 
 @onready var back_to_game_button: Button = %BackToGameButton
 @onready var save_and_quit_button: Button = %SaveAndQuitButton
+@onready var volume: HSlider = %Volume
+@onready var mute: CheckBox = %Mute
+@onready var option_button: OptionButton = %OptionButton
 
+var pause_time: float
 
 func _ready() -> void:
 	back_to_game_button.pressed.connect(_unpause)
 	save_and_quit_button.pressed.connect(_on_save_and_quit_button_pressed)
+
+
+func _process(delta: float) -> void:
+	var volume_linear 
+	var target_volume
+	for player: AudioStreamPlayer in MusicPlayer.get_children():
+		volume_linear = db_to_linear(player.volume_db)
+		target_volume = 0.0 if get_tree().paused else 1.0
+		volume_linear = lerp(volume_linear, target_volume, 8.0 * delta)
+		player.volume_db = linear_to_db(volume_linear)
+		player.stream_paused = get_tree().paused and Time.get_ticks_msec() > pause_time + 300
 
 
 func _shortcut_input(event: InputEvent) -> void:
@@ -23,6 +38,7 @@ func _shortcut_input(event: InputEvent) -> void:
 func _pause() -> void:
 	show()
 	get_tree().paused = true
+	pause_time = Time.get_ticks_msec()
 
 
 func _unpause() -> void:
@@ -33,3 +49,35 @@ func _unpause() -> void:
 func _on_save_and_quit_button_pressed() -> void:
 	get_tree().paused = false
 	save_and_quit.emit()
+
+
+func _on_volume_value_changed(value: float) -> void:
+	if value == 0:
+		AudioServer.set_bus_mute(0, true)
+	else:
+		AudioServer.set_bus_mute(0, false)
+		AudioServer.set_bus_volume_db(0, value / 8)
+
+
+func _on_music_volume_value_changed(value: float) -> void:
+	for music: AudioStreamPlayer in SFXPlayer.get_children():
+		music.volume_db = value
+
+
+func _on_sfx_volume_value_changed(value: float) -> void:
+	for sfx: AudioStreamPlayer in SFXPlayer.get_children():
+		sfx.volume_db = value
+
+
+func _on_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0, toggled_on)
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	match index:
+		0: DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1: DisplayServer.window_set_size(Vector2i(1600, 900))
+		2: DisplayServer.window_set_size(Vector2i(1280, 720))
+
+func _on_window_mode_option_button_item_selected(index: int) -> void:
+	DisplayServer.window_set_mode(option_button.get_item_id(index))
